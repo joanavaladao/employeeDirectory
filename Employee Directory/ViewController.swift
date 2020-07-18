2c3eeb51b7b2ac9f0a53a1526594f019e853ac81
Joana Valadão <jvaladao@touchbistro.com>
2020-07-18 08:18:49 -0400

    Add files via upload

---
+++ Employee Directory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Employee Directory
+//
+//  Created by Joana Valadao on 2020-07-18.
+//  Copyright © 2020 Joana Valadao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Employee DirectoryTests/Employee_DirectoryTests.swift
@@ -0,0 +1,34 @@
+//
+//  Employee_DirectoryTests.swift
+//  Employee DirectoryTests
+//
+//  Created by Joana Valadao on 2020-07-18.
+//  Copyright © 2020 Joana Valadao. All rights reserved.
+//
+
+import XCTest
+@testable import Employee_Directory
+
+class Employee_DirectoryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
