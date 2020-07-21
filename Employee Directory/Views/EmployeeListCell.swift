20d495ef54ef98a31c02748d09a8d9f4046f4bc1
Joana <joanavaladao@gmail.com>
2020-07-20 23:01:16 -0400

    Collection view + custom cell + navigation bar (1st screen)

--- Employee Directory/Views/EmployeeListCell.swift
+++ Employee Directory/Views/EmployeeListCell.swift
@@ -10,4 +10,77 @@ import UIKit
 
 class EmployeeListCell: UICollectionViewCell {
     
+    lazy var photo: UIImageView = {
+        let view = UIImageView(frame: .zero)
+        view.image = UIImage(named: "person")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var name: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "NAME"
+        label.font = UIFont.systemFont(ofSize: 17.0)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var team: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "TEAM"
+        label.font = UIFont.systemFont(ofSize: 13.0)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var stackLabel: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [name, team])
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.distribution = .fillEqually
+        stack.spacing = 8.0
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [photo, stackLabel])
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.distribution = .fillProportionally
+        stack.spacing = 16.0
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
+
+private extension EmployeeListCell {
+    func setupView() {
+        backgroundColor = .white
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 8.0),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8.0),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8.0),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8.0),
+            
+            photo.heightAnchor.constraint(equalToConstant: 139.5),
+            photo.widthAnchor.constraint(equalToConstant: 139.0)
+        ])
+    }
+}
+
--- Employee Directory/Views/EmployeeListViewController.swift
+++ Employee Directory/Views/EmployeeListViewController.swift
@@ -19,11 +19,22 @@ class EmployeeListViewController: UIViewController {
         layout.scrollDirection = .vertical
         let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collection.translatesAutoresizingMaskIntoConstraints = false
-        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collection.register(EmployeeListCell.self, forCellWithReuseIdentifier: "cell")
         collection.delegate = self
         collection.dataSource = self
         return collection
     }()
+    
+    lazy var navigationBar: UINavigationBar = {
+        let bar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 44))
+        let item = UINavigationItem(title: "Employee Directory")
+        item.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: nil, action: #selector(refresh))
+        bar.setItems([item], animated: true)
+        bar.translatesAutoresizingMaskIntoConstraints = false
+        return bar
+    }()
+    
+    private let viewModel: EmployeeListViewModel = EmployeeListViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,28 +45,43 @@ class EmployeeListViewController: UIViewController {
 private extension EmployeeListViewController {
     func setupView() {
         view.backgroundColor = .white
+        view.addSubview(navigationBar)
         view.addSubview(collectionView)
         collectionView.backgroundColor = .gray
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navigationBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navigationBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            navigationBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor),
             collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
     }
+    
+    @objc func refresh() {
+        
+    }
 }
 
-extension EmployeeListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension EmployeeListViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 11
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .yellow
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 155.0, height: 230.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    }
     
 }
 
